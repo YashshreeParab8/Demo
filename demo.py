69468c58ddad54bea18703af69eb8ccaf919261a
YashshreeParab8 <yashshreeparab192@gmail.com>
2026-09-10 13:58:37 +0530

    Version 1.0.0

---
+++ demo.py
@@ -0,0 +1,9 @@
+#a1 = 10
+#b1 = 20
+#c1 = a1 + b1
+#print(c1)
+
+a = input("Enter your number:")
+b = 20
+c = a + b
+print(c)
---
+++ demoo.py
@@ -0,0 +1,29 @@
+
+#a = int(input("Enter a number:"))
+#print('The number is odd:',a%2!=0)
+
+#Write a program to print age in days
+#a  =  int(input("Enter your age:"))
+#print(f" {a} years = {a*365} days ")
+
+#Write a Program to convert minutes into hours and 
+#minutes = int(input("Enter minutes:"))
+#print(f" {minutes} is {minutes//60} hours {minutes%60} minutes")
+
+#write a program to extract last digit of an number
+#no = int(input("Enter a number:"))
+#print(f" {no} : last digit is {no%10} ")
+#or
+#no = input("Enter a number:")
+#print(f" {no} : last digit is {no[-1]} ")
+
+#write a program to check if a person is eligible for discount the criteria is he must be a student and age mst be below 21
+role = input("Enter your role(student/teacher):")
+age = int(input("Enter your age:"))
+print(f" Eligible: {age<21 and role == 'student'}")
+
+#write a program to swap two numbers without 3rd variable and using arithmatic operations
+#a = 23
+#b = 45
+#print(f"before swap: {a}= 10 ")
+#Yashashreeparab 
