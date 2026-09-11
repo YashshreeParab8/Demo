89a2c104a3570cc63f7cf1668183d5b09ee94a08
YashshreeParab8 <yashshreeparab192@gmail.com>
2026-09-11 13:33:33 +0530

    Conditional Programs

--- demoo.py
+++ demoo.py
@@ -18,9 +18,9 @@
 #print(f" {no} : last digit is {no[-1]} ")
 
 #write a program to check if a person is eligible for discount the criteria is he must be a student and age mst be below 21
-role = input("Enter your role(student/teacher):")
-age = int(input("Enter your age:"))
-print(f" Eligible: {age<21 and role == 'student'}")
+#role = input("Enter your role(student/teacher):")
+#age = int(input("Enter your age:"))
+#print(f" Eligible: {age<21 and role == 'student'}")
 
 #write a program to swap two numbers without 3rd variable and using arithmatic operations
 #a = 23
@@ -29,3 +29,152 @@ print(f" Eligible: {age<21 and role == 'student'}")
 #Yashashreeparab 
 #xyz
 #okp
+
+#11/9
+#Conditional statements
+#is_raining = False
+#if is_raining:
+#    print("raining outside")
+#else :
+#    print("not raining")
+
+#age = int(input("Enter your age:"))
+#if age<=18 :
+#    print("Not Eligible for voting")
+#else :
+#    print("Eligible for voting")
+
+
+#n=int(input("Enter a number:"))
+#if n==1:
+#     print("Sunday")
+#elif n==2:
+#   print("Monday")
+#elif n==3:
+#    print("Tuesday")
+#elif n==4:
+#    print("Wednesday")
+##elif n==5:
+#    print("Thursday")
+#elif n==6:
+ #   print("Friday")
+#elif n==7:
+ #   print("Saturday")
+#else:
+ #   print("Invalid number")
+
+
+#age = 20
+#has_id = True
+#if age >= 18:
+#    if has_id:
+#        print("Entry allowed")
+#    else:
+#        print("Id required")
+#else:
+#    print("Underage")
+
+#MATCH CASE
+#day = int(input("Enter the day number:"))
+
+#match day:
+#    case 1:
+#        print("Monday")
+#    case 2:
+#        print("Tuesday")
+#    case 3:
+#        print("Wednesday")
+#    case 4:
+ #       print("Thursday")
+  #  case 5:
+   #     print("friday")
+   # case 6:
+   #     print("saturday")
+   # case 7:
+   #     print("sunday")
+   # case _:
+   #     print("invalid number")
+
+#odd even
+#n = int(input("Enter a number:"))
+#if n%2==0 :
+#    print("The number is even")
+#else:
+#    print("Number is odd")
+
+
+#age = int(input("Enter age:"))
+#price = 500
+#if age<12 :
+#    print("10% Discount Applies | Ticket price =", (price)-(price*10/100))
+#else :
+#   print("Discount does not apply | Ticket price=",age)
+
+
+#marks above 90 o grade 80 pl a+ till f
+#marks = int(input("Enter marks:"))
+#if marks>=90 and marks<100 :
+#    print("Grade:O")
+#elif marks>=80 and marks<=90 :
+#    print("Grade:A")
+#elif marks>65 and marks<=80 :
+#    print("Grade:B")
+#elif marks<=35 and marks>=0:
+#    print("Grade:C|failed")
+#else :
+#    print("Invalid marks")
+
+#check positiove negative
+#no= int(input("Enter a number:"))
+#if no<0 :
+#    print("Number is negative")
+#elif no>0:
+#    print("Number is positive")
+#else:
+#    print("Number is 0")
+
+
+#a= int(input("Enter first number:"))
+#b = int(input("Enter second number:"))
+#c = int(input("ENter third number:"))
+
+#if a>=b and a>=c:
+ #   print(a)
+
+#elif b>=a and b>=c:
+#    print(b)
+
+#elif a==b==c:
+#    print(a)
+
+#else:
+#    print(c)
+
+#year = int(input("Enter year:"))
+#if year%4==0:
+#    print("Leap year")
+#else:
+#    print("Not leap year")
+
+#calculator
+a=int(input("Enter first no-"))
+b=int(input("Enter second no-"))
+choice=int(input(("1-addition\n2-subtraction\n3-multiplication\n4-division\nEnter your choice:")))
+
+match choice:
+    case 1: 
+            print("addition=",a+b)
+
+    case 2: 
+            print("Subtraction=",a-b)
+
+    case 3: 
+            print("Multiplication=",a*b)
+
+    case 4: 
+              if b==0:
+                print("Division by 0 not possible")
+              else:
+                print("Division=",a/b)
+
+    case _: print("invalid choice")
